a3c462cdc5cfb8ba22c7fd1f07a523a4a80e9339
B.Thompson <bernardsthompson@gmail.com>
2023-04-11 12:03:05 -0700

    update

--- archi/prepdisk.sh
+++ archi/prepdisk.sh
@@ -2,14 +2,7 @@
 ##B.Thompson
 ##disk mangler
 source archi.conf
-parted --script -a optimal -- "$rdev" \
-	mklabel gpt \
-	mkpart primary 1MiB 1024MiB \
-	mkpart primary 1024MiB 2048MiB \
-	mkpart primary 2048MiB -2048s 
-
+parted --script -a optimal -- "$rdev" mklabel gpt mkpart primary 1MiB 1024MiB mkpart primary 1024MiB 2048MiB mkpart primary 2048MiB -2048s 
 if [ "$fpdisk" = "true" ]; then
-parted --script -a optimal -- "$bdev" \
-	mklabel gpt \
-	mkpart primary 1MiB -2048s 
+parted --script -a optimal -- "$fpdev" mklabel gpt mkpart primary 1MiB -2048s 
 fi
