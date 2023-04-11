777e702b2001c03b5c7213cf971c2abecf9946fe
B.Thompson <bernardsthompson@gmail.com>
2023-04-11 12:05:00 -0700

    update

--- archi/prepdisk.sh
+++ archi/prepdisk.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 ##B.Thompson
-##disk mangler
 source archi.conf
 parted --script -a optimal -- "$rdev" mklabel gpt mkpart primary 1MiB 1024MiB mkpart primary 1024MiB 2048MiB mkpart primary 2048MiB -2048s 
 if [ "$fpdisk" = "true" ]; then
